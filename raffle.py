82f545d813dceb0e359e7e28c70004fd9d49b919
vagrant <vagrant@vagrant.vm>
2018-01-11 04:29:13 +0000

    Fixed import error, completed version 2

--- raffle.py
+++ raffle.py
@@ -42,7 +42,7 @@ def get_customers_from_file(customer_file_path):
 def pick_winner(customers):
     """Choose a random winner from list of customers."""
 
-    chosen_customer = random.choice(customers)
+    chosen_customer = choice(customers)
 
     print "Tell {name} at {email} that they've won".format(
         name=chosen_customer.name,
@@ -55,3 +55,5 @@ def run_raffle():
 
     customers = get_customers_from_file("customers.txt")
     pick_winner(customers)
+
+run_raffle()
--- version2/raffle.py
+++ version2/raffle.py
@@ -4,3 +4,25 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+from random import choice
+from customers import get_customers_from_file
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    print "Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        )
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+run_raffle()
